767b7834bb8ca5980629e878279a9d205f6f80dd
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2026-06-18 13:45:13 +0100

    Setup Java Maven setup

---
+++ src/main/java/PRReviewTaskApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PRReviewTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PRReviewTaskApplication.class, args);
+    }
+}
